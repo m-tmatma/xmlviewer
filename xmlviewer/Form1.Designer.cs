67b018dd92d6f3b9ec2062fec2caaeff2189f1c0
Masaru Tsuchiyama <m.tmatma@gmail.com>
2022-09-17 19:34:29 +0900

    add treeview

--- xmlviewer/Form1.Designer.cs
+++ xmlviewer/Form1.Designer.cs
@@ -28,12 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtXmlFile = new System.Windows.Forms.TextBox();
+            this.xmlReadButton = new System.Windows.Forms.Button();
+            this.treeXml = new System.Windows.Forms.TreeView();
+            this.SuspendLayout();
+            // 
+            // txtXmlFile
+            // 
+            this.txtXmlFile.Location = new System.Drawing.Point(59, 30);
+            this.txtXmlFile.Name = "txtXmlFile";
+            this.txtXmlFile.Size = new System.Drawing.Size(1694, 39);
+            this.txtXmlFile.TabIndex = 0;
+            // 
+            // xmlReadButton
+            // 
+            this.xmlReadButton.Location = new System.Drawing.Point(1792, 26);
+            this.xmlReadButton.Name = "xmlReadButton";
+            this.xmlReadButton.Size = new System.Drawing.Size(150, 46);
+            this.xmlReadButton.TabIndex = 1;
+            this.xmlReadButton.Text = "button1";
+            this.xmlReadButton.UseVisualStyleBackColor = true;
+            this.xmlReadButton.Click += new System.EventHandler(this.xmlReadButton_Click);
+            // 
+            // treeXml
+            // 
+            this.treeXml.Location = new System.Drawing.Point(75, 142);
+            this.treeXml.Name = "treeXml";
+            this.treeXml.Size = new System.Drawing.Size(1678, 1075);
+            this.treeXml.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1985, 1276);
+            this.Controls.Add(this.treeXml);
+            this.Controls.Add(this.xmlReadButton);
+            this.Controls.Add(this.txtXmlFile);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txtXmlFile;
+        private Button xmlReadButton;
+        private TreeView treeXml;
     }
 }
--- xmlviewer/Form1.cs
+++ xmlviewer/Form1.cs
@@ -1,3 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
 namespace xmlviewer
 {
     public partial class Form1 : Form
@@ -6,5 +18,69 @@ namespace xmlviewer
         {
             InitializeComponent();
         }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtXmlFile.Text = Path.Combine(Application.StartupPath, @"../../pom.xml");
+        }
+
+        private void xmlReadButton_Click(object sender, EventArgs e)
+        {
+            treeXml.Nodes.Clear();
+            XmlDocument doc = new XmlDocument();
+            try
+            {
+                doc.Load(txtXmlFile.Text);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+                return;
+            }
+
+            ConvertXmlNodeToTreeNode(doc, treeXml.Nodes);
+            treeXml.Nodes[0].ExpandAll();
+        }
+
+        private void ConvertXmlNodeToTreeNode(XmlNode xmlNode, TreeNodeCollection treeNodes)
+        {
+            TreeNode newTreeNode = treeNodes.Add(xmlNode.Name);
+            switch (xmlNode.NodeType)
+            {
+                case XmlNodeType.ProcessingInstruction:
+                case XmlNodeType.XmlDeclaration:
+                    newTreeNode.Text = "<?" + xmlNode.Name + ">";
+                    break;
+                case XmlNodeType.Element:
+                    newTreeNode.Text = "<" + xmlNode.Name + ">";
+                    break;
+                case XmlNodeType.Attribute:
+                    newTreeNode.Text = "ATRIBUTE:" + xmlNode.Name;
+                    break;
+                case XmlNodeType.Text:
+                case XmlNodeType.CDATA:
+                    newTreeNode.Text = xmlNode.Value;
+                    break;
+                case XmlNodeType.Comment:
+                    newTreeNode.Text = "<!--" + xmlNode.Value + "-->";
+                    break;
+            }
+
+
+            if (xmlNode.Attributes != null)
+            {
+                foreach (XmlAttribute attribute in xmlNode.Attributes)
+                {
+                    ConvertXmlNodeToTreeNode(attribute, newTreeNode.Nodes);
+                }
+            }
+
+            foreach (XmlNode childNode in xmlNode.ChildNodes)
+            {
+                ConvertXmlNodeToTreeNode(childNode, newTreeNode.Nodes);
+            }
+        }
+
     }
 }
